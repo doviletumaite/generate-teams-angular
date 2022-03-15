908c5e121687dbd6b8fa03e57af868cb8314ab1f
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-03-15 18:25:50 +0100

    starting point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+  newMemberName = "";
+  members:string[] = []
+
+  addMember(){
+    console.log("click")
+  }
 }
